4aaa1942f0af79ccc3b1e98627626fa410f748df
Anders Ryden <rydena021@gmail.com>
2018-11-13 13:02:47 -0600

    adds vega  route

--- server/server.js
+++ server/server.js
@@ -4,7 +4,15 @@ let app = express();
 const PORT = 5000;
 
 app.use(express.static('server/public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// listen for localhost:5000/vega
+app.get('/vega', function (request, response) {
+  // say hello to vega
+  response.send('hello world')
+});
+
 app.listen(PORT, function () {
   console.log('app is running');
-})
+});
